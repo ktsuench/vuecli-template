a300fa6980cffb9c78a3694235113d310b2a518a
ktsuench <will289@hotmail.com>
2019-12-31 00:32:59 -0500

    refactor: moved data passed to list.props.items to data fn

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <message-toggle />
-    <list :items="['list item 1', 'list item 2', 'list item 3']" />
+    <list :items="listItems" />
   </div>
 </template>
 
@@ -12,6 +12,9 @@ import MessageToggle from '@/components/MessageToggle.vue';
 import List from '@/components/List.vue';
 export default {
   name: 'home',
+  data: () => ({
+    listItems: ['list item 1', 'list item 2', 'list item 3'],
+  }),
   components: {
     MessageToggle,
     List,
