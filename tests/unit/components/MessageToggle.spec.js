0373b90ac0b78f8e72e8933abb6e2021bfdb59b5
ktsuench <will289@hotmail.com>
2019-12-31 00:30:40 -0500

    test: added check for props created from data fn

--- tests/unit/components/MessageToggle.spec.js
+++ tests/unit/components/MessageToggle.spec.js
@@ -1,17 +1,29 @@
+import Vue from 'vue';
 import { shallowMount } from '@vue/test-utils';
 import MessageToggle from '@/components/MessageToggle.vue';
 import Message from '@/components/Message.vue';
 
 describe('MessageToggle.vue', () => {
-  it('toggles msg passed to Message when button is clicked', () => {
+  describe('data.msg property', () => {
+    const wrapper = shallowMount(MessageToggle);
+    const msgData = wrapper.vm.$data.msg;
+
+    it('is null', () => {
+      expect(msgData).toBeNull();
+    });
+  });
+
+  it('toggles msg passed to Message when button is clicked', async () => {
     const wrapper = shallowMount(MessageToggle);
     const button = wrapper.find('#toggle-message');
     const MessageComponent = wrapper.find(Message);
 
     button.trigger('click');
+    await Vue.nextTick();
     expect(MessageComponent.props()).toEqual({ msg: 'message' });
 
     button.trigger('click');
+    await Vue.nextTick();
     expect(MessageComponent.props()).toEqual({ msg: 'toggled message' });
   });
 
