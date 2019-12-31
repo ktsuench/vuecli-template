fef2b85891deaa1334090f795a0a6c40d3a53152
ktsuench <will289@hotmail.com>
2019-12-30 23:50:34 -0500

    chore: updated jest roots to src and tests dirs

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,31 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/**/*.{js,vue}',
     '!src/**/*.spec.js',
     '!src/vendor/*.js',
+    '!src/main.js',
+    'src/**/*.{js,vue}',
   ],
-  // coverageThreshold: {
-  //   global: {
-  //     statements: 100,
-  //     branches: 100,
-  //     functions: 100,
-  //     lines: 100,
-  //   },
-  // },
+  coverageThreshold: {
+    global: {
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
   coverageDirectory: 'coverage',
   coverageReporters: ['html', 'text-summary'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/.history/',
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/.history/',
+  ],
 };
